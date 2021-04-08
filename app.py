948b37ad405f351d986fba1370d223793c62467f
Danny Rojas reyes <afarable-1997@hotmail.com>
2021-04-08 12:49:51 -0500

    Fetch api data to store in a dict

--- app.py
+++ app.py
@@ -5,10 +5,25 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
 """ Return render with context (weather_data) """
-@app.route('/weather')
+
+@app.route('/weather', methods=['GET', 'POST'])
 def index():
     url = environ.get('END_POINT_UNITS')
     city = request.form.get('city')
+    if city == '':
+        city = "Medellin"
+    res = requests.get(url.format(city)).json()
+    
+    weather = {
+        'city' : city,
+        'temperature' : res['main']['temp'],
+        'description' : res['weather'][0]['description'],
+        'icon' : res['weather'][0]['icon'],
+    }
+ 
+    weather_data = []
+    weather_data.append(weather)
 
     return render_template('/index.html')
